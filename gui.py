06f83cc2f4ee7de4eeedca0a678af446220fec87
justin <just.ecarma@gmail.com>
2022-04-23 13:30:03 -0400

    trying to put an entry box in the popup

--- gui.py
+++ gui.py
@@ -12,8 +12,9 @@ root.geometry("200x150")
 mainFrame = Frame(root)
 mainFrame.grid()
 
-def closeWindow():
+def closeMainFrame():
   root.destroy()
+
 
 def encryptButtonClicked():
   filename = askopenfilename(initialdir = "/", title = "Select a File", filetypes = (("Text files", "*.txt*"), ("all files", "*.*")))
@@ -24,14 +25,25 @@ def decryptButtonClicked():
 
 def keygenButtonClicked():
   top = Toplevel(root)
-  top.geometry("200x150")
+  top.geometry("400x400")
   top.title("Key Generation")
+
+  def closeKeyGenWindow():
+    top.destroy()
+
+  textEntry = StringVar()
+
+  e = ttk.Entry(top, textvariable=textEntry, width=10)
+
   Label(top, text= "Enter your key's name").place(x=20,y=50)
+  e.place(x=20, y=100)
+  confirm_button = Button (top, text="Confirm", command=closeKeyGenWindow)
+
         
 encrypt_button = Button (root, text = "Encrypt", command = encryptButtonClicked)
 decrypt_button = Button (root, text = "Decrypt", command = decryptButtonClicked)
 keygen_button = Button(root, text="Generate Key", command=keygenButtonClicked)
-close_button = Button (root, text = "Close", command = closeWindow)
+close_button = Button (root, text = "Close", command = closeMainFrame)
 
 encrypt_button.place(x=20, y=50)
 decrypt_button.place(x=20, y=80)
