a4ff15e175a163ceea3b1614b0579e739bfd8323
shiddha4 <suni0211@gmail.com>
2022-04-23 11:35:26 -0400

    message

--- encrypt.py
+++ encrypt.py
@@ -20,18 +20,6 @@ def main(filename, key_ID):
 
 
 
-def decrypt(filename, key):
-    with open(key + ".key", 'rb') as filekey:
-        key = filekey.read()
-
-    fernet = fnet(key)
-    with open(filename, 'rb') as file:
-        original = file.read()
-
-    decrypt = fernet.decrypt(original)
-
-    with open(filename, 'wb') as encrypted_file:
-        encrypted_file.write(decrypt)
 
 
 if __name__ == "__main__":  # init
