365c135bcf7c49f4e9f6a95e170e0c64aadb7921
justin <just.ecarma@gmail.com>
2022-04-23 11:28:36 -0400

    merging ian's additions and updating how the window is made

--- gui.py
+++ gui.py
@@ -3,6 +3,10 @@ from tkinter.ttk import *
 
 root = Tk()
 
+root.geometry("200x150")
+frame = Frame(root)
+frame.grid()
+
 def encryptButtonClicked():
   filename = fdialogue.askopenfilename(initialdir = "/", title = "Select a File", filetypes = (("Text files", "*.txt*"), ("all files", "*.*")))
   #Connect encryption code here
@@ -10,7 +14,7 @@ def encryptButtonClicked():
 def decryptButtonClicked():
   filename = fdialogue.askopenfilename(initialdir = "/", title = "Select a File", filetypes = (("Text files", "*.txt*"), ("all files", "*.*")))
   #Connect decryption code here
-  
+
 root.title("Deep_Crypt File Service")
         
 encrypt_button = Button (root, text = "Encrypt", command = encryptButtonClicked)
